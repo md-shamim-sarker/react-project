5812b4c0064a7544586f90e90f36c1448f2555d3
shamimspro <shamimspro@gmail.com>
2022-10-14 15:06:26 +0600

    github authentication

--- src/components/GithubAuthentication.js
+++ src/components/GithubAuthentication.js
@@ -1,9 +1,47 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {getAuth, signInWithPopup, GithubAuthProvider, signOut} from "firebase/auth";
+import app from '../firebase/Firebase.init';
+import FileLocation from './FileLocation';
+
+const provider = new GithubAuthProvider();
+const auth = getAuth(app);
 
 const GithubAuthentication = () => {
+    const [user, setUser] = useState({});
+
+    const signInHandler = () => {
+        signInWithPopup(auth, provider)
+            .then((result) => {
+                const user = result.user;
+                setUser(user);
+                console.log(user);
+            }).catch((error) => {
+                console.log(error);
+            });
+    };
+
+    const signOutHandler = () => {
+        signOut(auth).then(() => {
+            setUser({});
+        }).catch((error) => {
+            console.log(error);
+        });
+    };
+
     return (
-        <div>
-            github
+        <div className='flex flex-col items-center'>
+            {
+                user.uid
+                    ? <button onClick={signOutHandler}>Sign Out</button>
+                    : <button onClick={signInHandler}>Sign In</button>
+            }
+            {
+                user.uid && <>
+                    <h2>Name: {user.displayName}</h2>
+                    <img src={user.photoURL} alt="user_photo" />
+                </>
+            }
+            <FileLocation file={"src/components/GithubAuthentication.js"}></FileLocation>
         </div>
     );
 };
